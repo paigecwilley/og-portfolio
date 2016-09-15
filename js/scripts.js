dd1c0e71ed3637987c15de5ddc7b152e2602eb5e
Paige C Willey <paige.catherine.willey@gmail.com>
2016-09-14 22:19:42 -0600

    Added js, updated nav, added about section

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	console.log('hello');
+	$('.nav-default li a').click(function(){
+			$('li a').removeClass("active");
+			$(this).addClass("active");
+			console.log('something is happening');
+
+
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
